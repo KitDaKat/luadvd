546b63655746bcbb5ddb1ca21f98befdb3c35c4c
KitDaKat <koolkid1234123412341234@gmail.com>
2025-02-25 21:34:00 -0500

    added sound for dvd hit, and made it run the same regardless of fps v1.1

--- main.lua
+++ main.lua
@@ -5,6 +5,9 @@ function love.load()
     NewFont = love.graphics.newFont("Roboto-Regular.ttf", 30)
     love.graphics.setFont(NewFont)
 
+    Hitaudio = love.audio.newSource("Hit.mp3", "static")
+    Muted = false
+
     Paused = false
 
     love.window.setFullscreen(true)
@@ -16,8 +19,8 @@ function love.load()
         ["XPosition"] = 10,
         ["YPosition"] = 10,
         ["radius"] = 50,
-        ["ChangeX"] = 1,
-        ["ChangeY"] = 1,
+        ["ChangeX"] = 480,
+        ["ChangeY"] = 480,
     
     }
 
@@ -29,6 +32,11 @@ function love.load()
         if key == "w" then 
             if love.window.getFullscreen() == true then love.window.setFullscreen(false) else love.window.setFullscreen(true) end
         end
+
+        if key == "m" then 
+            if Muted == true then Muted = false else Muted = true end
+        end
+
     end
 
     function love.mousepressed(x, y, button, istouch, presses)
@@ -47,13 +55,16 @@ function love.update(dt)
 
 if Paused == false then
 
-    DVD["XPosition"] = DVD["XPosition"] + DVD["ChangeX"]
-    DVD["YPosition"] = DVD["YPosition"] + DVD["ChangeY"]
+    DVD["XPosition"] = DVD["XPosition"] + (DVD["ChangeX"] * dt)
+    DVD["YPosition"] = DVD["YPosition"] + (DVD["ChangeY"] * dt)
 
     if DVD["XPosition"] - 85 <= 0 or DVD["XPosition"] + 85 >= love.graphics.getWidth() then
 
         if DVD["XPosition"] - 85 <= 0 then DVD["XPosition"] = 85 else DVD["XPosition"] = love.graphics.getWidth() - 85 end
         DVD["ChangeX"] = 0 - DVD["ChangeX"]
+        if Muted == false then
+        love.audio.play(Hitaudio)
+        end
 
     end
 
@@ -61,6 +72,9 @@ if Paused == false then
             
         if DVD["YPosition"] - 55 <= 0 then DVD["YPosition"] = 55 else DVD["YPosition"] = love.graphics.getHeight() - 55 end
         DVD["ChangeY"] = 0 - DVD["ChangeY"]
+        if Muted == false then
+        love.audio.play(Hitaudio)
+        end
 
     end
 
@@ -79,7 +93,7 @@ function love.draw()
     if Paused == true then 
         local font = love.graphics:getFont()
         love.graphics.print("paused", love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, 1, 1, font:getWidth("paused")/2, font:getHeight()/2)
-        love.graphics.print("w - toggle fullscreen \n escape - pause/unpause \n backspace - close", 50, 50, 0, 0.5, 0.5, font:getWidth("w - toggle fullscreen \n escape - pause/unpause")/4, font:getHeight()/4)
+        love.graphics.print("w - toggle fullscreen \n escape - pause/unpause \n m - mute audio", 50, 50, 0, 0.5, 0.5, font:getWidth("w - toggle fullscreen \n escape - pause/unpause")/4, font:getHeight()/4)
         love.graphics.print("click to open github page", 50, 150, 0, 0.5, 0.5, font:getWidth("click to open github page")/4, font:getHeight()/4)
     end
 
